45f64deed54a340f3ab2b8e99ec2d2bc0fe41fb8
DESKTOP-B2QPHF7\lucia <luciahrovatin@gmail.com>
2021-10-18 15:52:53 +0200

    twitter data

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from Collectors import CrushCollector, EsnCollector
+#from Collectors import CrushCollector, EsnCollector
+from twitter import twitterAPI
 
 if __name__ == "__main__":
-        try :
+        try:
             cc = CrushCollector()
             cc.visit_event_and_write()
 
+
             esn = EsnCollector()
             esn.visit_event_and_write()
+
+            # Twitter
+            #visit_trentino = twitterAPI()
+            #visit_trentino.parsing_tweets()
+
         except:
                 print('Error in page building encountered, please check if the link is correct')
 
--- twitter.py
+++ twitter.py
@@ -1,35 +1,57 @@
 import json
 import tweepy as tw
 
-# Change these personal keys
-CONSUMER_KEY = '46ue77KJ3l3ut1ITSPBb51csI'
-CONSUMER_SECRET = 'W4bbATMnaCRNe0QQqEhq98wTjgtRh6BdlkJRbdcz3IUSWQdjkK'
-ACCESS_TOKEN = '1385510822630330371-OcSJa7Ga1wpoG8NxGrDnBpBheaSM8A'
-ACCESS_TOKEN_SECRET = 'kOm9mTCIRWbt1Sft6rZNl3tfKlcyB8HjYF0DX7nQHImsC'
 
-# Instantiate a client
-auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tw.API(auth, wait_on_rate_limit=True)
+class twitterAPI:
 
-# Twitter API only allows you to access the past few weeks of tweets.
-search_words = "#visittrentino"
+    def __init__(self, search_word="#visittrentino"):
+        self.CONSUMER_KEY = '46ue77KJ3l3ut1ITSPBb51csI'
+        self.CONSUMER_SECRET = 'W4bbATMnaCRNe0QQqEhq98wTjgtRh6BdlkJRbdcz3IUSWQdjkK'
+        self.ACCESS_TOKEN = '1385510822630330371-OcSJa7Ga1wpoG8NxGrDnBpBheaSM8A'
+        self.ACCESS_TOKEN_SECRET = 'kOm9mTCIRWbt1Sft6rZNl3tfKlcyB8HjYF0DX7nQHImsC'
+        self.search_word = search_word
 
-tweets = tw.Cursor(api.search_tweets,
-                   q=search_words,
-                   #geocode="46.06787,11.1210,50km",  # geocode for trento +radius of 50 km
-                   tweet_mode='extended',
-                   lang="en").items(500)  # european languages
+    def search_tweets(self, n_tweets=500):
+        """
+        The function instantiates a client and downloads n tweets
+        containing the hashtag specified within the api.
+        """
+        # Instantiate a client
+        auth = tw.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+        auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_TOKEN_SECRET)
+        api = tw.API(auth, wait_on_rate_limit=True)
 
-output_list = []
-for tweet in tweets:
-    output_list.append(json.load(tweet._json))
+        # download info about 500 tweets
+        tweets = tw.Cursor(api.search_tweets,
+                           q=self.search_word,
+                           # geocode="46.06787,11.1210,50km",  # geocode for trento +radius of 50 km
+                           tweet_mode='extended',
+                           lang="en").items(n_tweets)  # european languages
+        return tweets
 
-all_items = []
-for json_file in output_list:
-    all_items += json_file['items']
+    def parsing_tweets(self):
+        """
+        Parsing only the interesting parts within the tweets.
+        :return:
+        """
+        tweets = self.search_tweets()
+        # save the tweets in a json file
+        output = []
 
-with open('twitter_data.json', 'w') as f:
-    json.dump({"items": all_items}, f, textfile_merged)
+        including_lst = ["created_at", "full_text", "entities", "metadata", "source",
+                         "geo","coordinates", "place", "contributors", "is_quote_status",
+                         "retweet_count", "favorite_count", "favorited", "retweeted", "lang"]
+
+        for tweet in tweets:
+            new_data = dict()
+            for item in tweet._json:
+                if item in including_lst:
+                    new_data[item] = tweet._json[item]
+            output.append(new_data)
+
+        with open('twitter_data.json', 'w') as f:
+            json.dump(output, f, indent=2)
+
+
 
 
