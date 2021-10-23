86a40efd6c37d860fcac20dd278289e53a261b54
AnnaFetz <annamaria.fetz@studenti.unitn.it>
2021-10-23 17:49:19 +0200

    jetn_reshaped

---
+++ jetn.py
@@ -0,0 +1,15 @@
+import os, csv, json 
+
+path = os.getcwd()
+jd = {}
+with open(os.path.join(path,'JETN_report_climbing_gap.csv'), encoding ="utf-8") as f :
+    dic = csv.DictReader(f, delimiter=';')
+   
+    for d in dic :
+        for k in d :
+            jd[k.replace('\ufeff','')] = d[k]  
+         
+            
+        with open('{}\{}.json'.format(path,'JETN_climbing_gap'), 'a', encoding='utf-8') as f:
+            json.dump(jd, f, ensure_ascii=False, indent=4, default=str)
+    
