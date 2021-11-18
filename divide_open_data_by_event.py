8bfe610d210ce6ee6aacfa09cb23b470372178ca
AnnaFetz <annamaria.fetz@studenti.unitn.it>
2021-11-18 23:20:09 +0100

    new files with lat lon and location type + event category + ticket

--- divide_open_data_by_event.py
+++ divide_open_data_by_event.py
@@ -3,8 +3,8 @@ import json
 data = r"C:\Users\Anna Fetz\Desktop\Data_Science\third_semester\KDI_2021\KDI_project\OpenData"
 dir = os.listdir(data) 
 save_dir = r"C:\Users\Anna Fetz\Desktop\Data_Science\third_semester\KDI_2021\KDI_project\OpenData\by_event"
-rovereto = os.path.join(data,dir[2])
-trento = os.path.join(data, dir[4])
+rovereto = os.path.join(data,'opendata_rovereto_parsed.json')
+trento = os.path.join(data, 'opendata_trento_parsed.json')
 
 
      
@@ -14,8 +14,8 @@ def entpack_city(file) :
         events = json.load(f)
        
         for e in events:
-            if ('nome' in e) : 
-                name = e['nome']
+            if ('title' in e) : 
+                name = e['title']
             else :
                 
                
@@ -25,5 +25,5 @@ def entpack_city(file) :
             with open(os.path.join(save_dir,name)+'.json', 'w', encoding = 'utf-8') as el :
                 json.dump(e, el, ensure_ascii=False, indent=4, default=str) 
 
-#entpack_city(rovereto)
+entpack_city(rovereto)
 entpack_city(trento)
---
+++ miniparsing.py
@@ -0,0 +1,92 @@
+import os 
+import json 
+import pprint as pp 
+dir = os.listdir(r'C:\Users\Anna Fetz\Desktop\Data_Science\third_semester\KDI_2021\EVENTS')
+
+d = {os.path.join(r'C:\Users\Anna Fetz\Desktop\Data_Science\third_semester\KDI_2021\EVENTS',v): [] for v in dir}
+
+
+def return_list_of_files(dir) : 
+    global d 
+    for current in d :
+        l = os.listdir(current)
+        for element in l :
+            if (element.endswith('.json')) :
+                d[current].append(os.path.join(current, element))
+            else :
+                l2 = os.listdir(os.path.join(current, element))
+                for other in l2 :
+                    d[current].append(os.path.join(os.path.join(current, element), other))
+
+    return d 
+
+def create_keys(dic) : 
+    key_words = {'b': set(['biglietto', 'prezzo','ticket', 'prenotazione','gratuito','ingresso libero','partecipazione','gratis','libero','ingresso', 'entrata']), 
+    'l':set(['webinar', 'online', 'zoom', 'teams'])}
+    refused = []
+    for key,val in dic.items() : 
+        for file in val :
+            if (file.endswith('.json')) :
+                try: 
+                    with open(file, encoding='utf-8') as f : 
+                        load  = json.load(f)
+                      
+                    load['is_online'] = False
+                    load['is_offline'] = False
+                    load['is_blended'] = False
+                    load['ticket'] = []
+                    if ('description' in load) : 
+                        load['description'] = load['description'].replace(r'\u0080', 'euro')
+                        if (load['name'][0] == '-') : 
+                            load['name'] = load['name'][1:]
+                        if ('party' in load['name']) :
+                            spl = 'party'
+                        else : 
+                            spl = load['name'].split('-')[0]
+                        load['category'] = spl
+
+                        desc = load['description'].split() 
+                        for word in desc : 
+                            n = word.lower().replace(':','')
+                            if (n in key_words['b']) and (load['ticket']== []) :
+                                
+                                i = desc.index(word)
+                                load['ticket'] = desc[i:] 
+                            if (n == 'euro') : 
+                                i = desc.index(word)
+                                load['ticket'].append(desc[i-1])
+                                load['ticket'].append(word)
+                                load['ticket'].append(desc[i+1]) 
+                            
+                            if (n in key_words['l']) : 
+                                i = desc.index(word)
+                                load['is_online'] = True 
+                            elif (n not in key_words['l']) : 
+                                load['is_offline'] = True 
+                    if ('ticket' not in load) :
+                        if ('info' in load) :  
+                            for word in desc : 
+                                n = word.lower()
+                                if (n in key_words['b']) :
+                                    i = desc.index(word)
+                                    load['ticket'] = desc[i:] 
+                                if (n in key_words['l']) : 
+                                    i = desc.index(word)
+                                    load['is_online'] = True 
+                                elif (n not in key_words['l']) : 
+                                    load['is_offline'] = True 
+                      
+                    with open(file, 'w', encoding = 'utf-8') as f2 :   
+                        json.dump(load, f2)
+                except :
+                    refused.append(file)
+    return refused
+        
+            
+        
+
+                            
+
+
+ret = create_keys(return_list_of_files(dir))
+print('LASTLY, PRINT RET {}'.format(ret))
