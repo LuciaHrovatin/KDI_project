90342c976bd36834a9e318c8e1dacfeb4090075b
Lucia Hrovatin <luciahrovatin@MBP-di-Lucia.lan>
2021-12-16 12:41:06 +0100

    changes_facility

--- codici.py
+++ codici.py
@@ -1,13 +1,38 @@
 import pandas as pd
-
+import json 
 
 fac = pd.read_csv("facility_final.csv")
 
-fac.drop(["Unnamed: 0.1", "information"], axis = 1, inplace=True)
+fac.drop(["Unnamed: 0.1", "information", "smoking"], axis = 1, inplace=True)
 fac.rename(columns={"has_location": "has_osmID"}, inplace=True)
 
-loc = pd.read_csv("location_final.csv")
+menu = []
 
+for x in fac.menu: # "{'cuisine': None, 'isVegetarian': None, 'isVegan': None, 'isGlutenFree': None}"
+    row = x.strip("{").strip("}").split()
+    add = ""
+    if row[3].strip(",") != "None" or row[5].strip(",") != "None" or row[-1] != "None":
+        if row[3].strip(",") != "None":
+            add += ", vegetarian"
+        if row[5].strip(",") != "None":
+            add += ", vegan"
+        if row[-1] != "None":
+            add += ", gluten free."
+    if add != "":
+        if row[1].strip(",") == "None":
+            m = add.lstrip(",") 
+        else:
+            m = row[1].strip(",") + add
+    elif row[1].strip(",") == "None":
+        m = None 
+    else: 
+        m = row[1].strip(",").strip("\'")
+    menu.append(m)
+
+fac["menu"] = menu
+
+
+loc = pd.read_csv("location_final.csv")
 loc.drop(["Unnamed: 0.1", "Unnamed: 0.1.1", "osm_ID"], axis = 1, inplace = True)
 
 loc.to_csv("locations.csv")
